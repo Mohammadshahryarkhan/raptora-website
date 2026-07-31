c9c12a2feaa7020922590e926176cb190ead022f
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-08-01 04:18:51 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -2,114 +2,67 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
 
     e.preventDefault();
 
-
     const email = document.getElementById("email").value.trim();
 
     const password = document.getElementById("password").value;
 
 
-
     try {
 
-
         const response = await fetch(
-            fetch("https://raptora-website-1.onrender.com/api/auth/login",
+            "https://raptora-website-1.onrender.com/api/auth/login",
             {
-
                 method: "POST",
 
                 headers: {
-
                     "Content-Type": "application/json"
-
                 },
 
                 body: JSON.stringify({
-
                     email: email,
-
                     password: password
-
                 })
-
             }
         );
-
 
 
         const data = await response.json();
 
 
-
         console.log("Server Response:", data);
 
 
+        if (response.ok) {
 
-        if(response.ok) {
+            localStorage.setItem("token", data.token);
 
+            localStorage.setItem("name", data.name);
 
-            // Store login details
+            localStorage.setItem("email", data.email);
 
-            localStorage.setItem(
-                "token",
-                data.token
-            );
-
-
-            localStorage.setItem(
-                "name",
-                data.name
-            );
-
-
-            localStorage.setItem(
-                "email",
-                data.email
-            );
-
-
-            localStorage.setItem(
-                "phone",
-                data.phone
-            );
-
+            localStorage.setItem("phone", data.phone);
 
 
             alert("Login Successful");
 
 
-
             window.location.href = "dashboard.html";
 
-
         }
-
 
         else {
 
-
             alert(data.message || "Login Failed");
-
 
         }
 
 
-
-    }
-
-
-    catch(error) {
-
+    } catch(error) {
 
         console.log("Error:", error);
 
-
-        alert(
-            "Backend connection failed. Start your server."
-        );
-
+        alert("Backend connection failed.");
 
     }
-
 
 });
