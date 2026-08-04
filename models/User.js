cffe08f447beb465316b5e833a09335edc58fadd
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-08-04 22:10:20 +0530

    Enhance user schema with password reset fields
    Added resetPasswordToken and resetPasswordExpires fields to user schema.

--- models/User.js
+++ models/User.js
@@ -2,24 +2,34 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
 
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
 
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true
     },
 
-    phone:{
-        type:String,
-        required:true
+    phone: {
+        type: String,
+        required: true
     },
 
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
+    },
+
+    resetPasswordToken: {
+        type: String,
+        default: null
+    },
+
+    resetPasswordExpires: {
+        type: Date,
+        default: null
     }
 
 });
