1f3e00908800f3d0d61b3c614d91c8a9ea649303
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-08-05 20:53:09 +0530

    Remove code block formatting from server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-```js
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -55,4 +55,4 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-```
+
