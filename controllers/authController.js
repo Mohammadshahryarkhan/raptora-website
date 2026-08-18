2ac02a6786173a0dec11bc5fb7cdbaa7322c75e8
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-08-18 15:08:24 +0530

    Change Gmail transporter to use port 587
    Updated Gmail transporter settings to use port 587 with TLS.

--- controllers/authController.js
+++ controllers/authController.js
@@ -171,21 +171,16 @@ exports.forgotPassword = async (req, res) => {
 
 
         // Gmail transporter
-              const transporter = nodemailer.createTransport({
-                   host: "smtp.gmail.com",
-                   port: 465,
-                  secure: true,
-
+             const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASSWORD
-
     }
-
 });
-
    
 
 
