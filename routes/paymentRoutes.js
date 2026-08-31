a53b0170d94a17f138b64f5a1a9b165e5079acaf
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-09-01 03:54:48 +0530

    Add new discount code PAPA99 with 99% value

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -79,6 +79,10 @@ const DISCOUNT_CODES = {
         type: "percentage",
         value: 100
     },
+     PAPA99: {
+        type: "percentage",
+        value: 99
+    },
 
     WELCOME50: {
         type: "fixed",
